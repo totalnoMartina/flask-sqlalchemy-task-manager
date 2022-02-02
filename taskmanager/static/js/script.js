da5dffd293692ab3007bd88bd02b4110d7721ce6
Martina Grganic <totalnobzvz@yahoo.com>
2022-02-02 22:05:05 +0000

    Add tasks template

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initialization
+    var sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+});
+
+// // Or with jQuery
+
+// $(document).ready(function() {
+//     $('.sidenav').sidenav();
+// });
