a8125d1f0a1fb3d3f37bb806bf83018f28e2c98f
Martina Grganic <totalnobzvz@yahoo.com>
2022-02-03 00:53:42 +0000

    Add tasks page, route, script

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -39,3 +39,18 @@ def delete_category(category_id):
     db.session.delete(category)
     db.session.commit()
     return redirect(url_for("categories"))
+
+@app.route("/add_task", methods=["GET", "POST"])
+def add_task():
+    if request.method == "POST":
+        task = Task(
+            ask_name=request.form.get('task_name'),
+            task_description=request.form.get('task_Description'),
+            is_urgent=bool(True if request.form.get("is_urgent") else False),
+            due_date=request.form.get("due_date"),
+            category_id=request.form.get("category_id")
+        )
+        db.session.add(task)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("add_task.html", categories=categories)
--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,10 +2,25 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+
+    // // Or with jQuery
+
+    // $(document).ready(function() {
+    //     $('.sidenav').sidenav();
+    // });
+
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {
+            done: "Select"
+        }
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
 });
-
-// // Or with jQuery
-
-// $(document).ready(function() {
-//     $('.sidenav').sidenav();
-// });
