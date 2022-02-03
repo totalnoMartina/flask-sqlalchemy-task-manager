6d17ecf34b2b9bc9933cc68f59d633e7684fac8c
Martina Grganic <totalnobzvz@yahoo.com>
2022-02-03 02:59:33 +0000

    Fix typo in __init__.py

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -9,13 +9,13 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
-if os.environ.get("DEVELOPMENT") == True:
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
 else:
-    uri = os.environ.get("DATABASE_URI")
+    uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
 db = SQLAlchemy(app)
 
