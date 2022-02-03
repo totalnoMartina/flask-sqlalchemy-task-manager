0f288e6cab6da245e892f5809c84a49ea4ef206a
Martina Grganic <totalnobzvz@yahoo.com>
2022-02-03 01:06:42 +0000

    Add tasks working properly

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -40,12 +40,14 @@ def delete_category(category_id):
     db.session.commit()
     return redirect(url_for("categories"))
 
+
 @app.route("/add_task", methods=["GET", "POST"])
 def add_task():
+    categories = list(Category.query.order_by(Category.category_name).all())
     if request.method == "POST":
         task = Task(
-            ask_name=request.form.get('task_name'),
-            task_description=request.form.get('task_Description'),
+            task_name=request.form.get("task_name"),
+            task_description=request.form.get("task_description"),
             is_urgent=bool(True if request.form.get("is_urgent") else False),
             due_date=request.form.get("due_date"),
             category_id=request.form.get("category_id")
